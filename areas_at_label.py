221c43a3a146cccfbbbb9cf86c7428d8a32085d5
Jeffrey Duda <jeff.duda@gmail.com>
2024-08-29 23:04:39 -0400

    ENH: get subject info from filename

--- areas_at_label.py
+++ areas_at_label.py
@@ -10,20 +10,27 @@ def main():
     parser.add_argument('-c', '--centroid', type=int, required=True, help='label to get slice from')
     parser.add_argument('-o', '--output', type=str, required=True, help='output csv file with areas')
     parser.add_argument('-d', '--dimension', type=int, default=2, required=False, help='dimension to get slice from')
-    parser.add_argument('-m', '--metadata', type=str, required=True, nargs=2, help='subject id and session id')
+    parser.add_argument('-m', '--metadata', type=str, required=True, help='subject_id session_id label_system')
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose output')
     args = parser.parse_args()
 
-    # Load the image
+    # Load the input images
     image = sitk.ReadImage(args.input)
     reference = sitk.ReadImage(args.reference)
 
+    naming=os.path.basename(args.input)
+    naming = naming.split('.')[0]
+    parts = naming.split('_')
+    series_name = parts[3:len(parts)-1]
+    series_name = "_".join(series_name)
+    #print(series_name)
+
+    # Get centroid of the reference label
     label_stats = sitk.LabelShapeStatisticsImageFilter()
     label_stats.Execute(reference)
     if not label_stats.HasLabel(int(args.centroid)):
         print('Label ' + str(args.centroid) + ' for centroid not found in reference image')
         sys.exit(1)
-
     centroid = label_stats.GetCentroid(int(args.centroid))
     slice_idx = reference.TransformPhysicalPointToIndex(centroid)[int(args.dimension)]
 
@@ -31,6 +38,7 @@ def main():
         print("Centroid (label="+str(args.centroid)+"): " + str(centroid))
         print("Slice (dim=" + str(args.dimension) + "): " + str(slice_idx))
 
+    # Extract single slice from label image
     index=[0, 0, slice_idx]
     size=[image.GetSize()[0], image.GetSize()[1], 0]
     if args.dimension == 0:
@@ -39,8 +47,9 @@ def main():
     elif args.dimension == 1:
         index=[0,slice_idx,0]
         size=[image.GetSize()[0],0,image.GetSize()[2]]
-
     slice = sitk.Extract(image, size=size, index=index)
+
+    # Get shape stats for labels in the slice
     slice_stats = sitk.LabelShapeStatisticsImageFilter()
     slice_stats.Execute(slice)
 
@@ -48,13 +57,18 @@ def main():
     label_list = [ x for sublist in args.labels for x in sublist ]
 
     dat=[]
+    # "id","accession","series_number","series_name","system","label","number","calculator","measure","metric","value"
     for l in label_list:
-        row={'subject': args.metadata[0], 'session': args.metadata[1], 'label':l, 'voxels':0, 'physical_area_mm':0.0, 'physical_volume_mm':0.0}
+        row1={'id': parts[0], 'accession': parts[1], 'series_number': parts[2], 'series_name': series_name, 'system': args.metadata, 'label':l, 'measure': 'shape', 'metric': 'nvoxels', 'value': 0}
+        row2={'id': parts[0], 'accession': parts[1], 'series_number': parts[2], 'series_name': series_name,'system': args.metadata, 'label':l, 'measure': 'shape', 'metric': 'physical_area_mm', 'value':0.0}
+        row3={'id': parts[0], 'accession': parts[1], 'series_number': parts[2], 'series_name': series_name,'system': args.metadata, 'label':l, 'measure': 'shape', 'metric': 'physical_volume_mm', 'value':0.0}
         if slice_stats.HasLabel(l):
-            row['voxels'] = slice_stats.GetNumberOfPixels(l)
-            row['physical_volume_mm'] = slice_stats.GetPhysicalSize(l)
-            row['physical_area_mm'] = slice_stats.GetPhysicalSize(l) / image.GetSpacing()[args.dimension]
-        dat.append(row)
+            row1['value'] = slice_stats.GetNumberOfPixels(l)
+            row2['value'] = slice_stats.GetPhysicalSize(l)
+            row3['value'] = slice_stats.GetPhysicalSize(l) / image.GetSpacing()[args.dimension]
+        dat.append(row1)
+        dat.append(row2)
+        dat.append(row3)
     
     df = pd.DataFrame(dat)
     if args.verbose:
